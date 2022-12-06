8f03146a945f4a143791cfad69d3a5d0ab970288
Quanndaph26528 <quanndaph26528@fpt.edu.vn>
2022-12-07 03:07:21 +0700

    upload

---
+++ DUAN1/BanTraSua_HT/src/main/java/poly/nhom4/service/TKDTService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package poly.nhom4.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+/**
+ *
+ * @author Admin
+ */
+public interface TKDTService {
+
+    List<Object[]> getAll();
+
+    List<Long> getHoaDon();
+
+    List<BigDecimal> getAllNow();
+
+    List<BigDecimal> getYear();
+
+    List< Object[]> getSP();
+
+    List<String> getSeller();
+}
